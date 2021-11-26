aa6cfe1c62177c00800e483d455816e067b05418
five-elements-system <93974491+five-elements-system@users.noreply.github.com>
2021-11-26 10:08:34 +0400

    Update fec.sol

--- fec.sol
+++ fec.sol
@@ -1,4 +1,4 @@
-// FECToken Contracts
+// SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.0;
 
@@ -17,10 +17,9 @@ import "./Context.sol";
  * https://forum.zeppelin.solutions/t/how-to-implement-erc20-supply-mechanisms/226[How
  * to implement supply mechanisms].
  *
- * We have followed general OpenZeppelin Contracts guidelines: functions revert
- * instead returning `false` on failure. This behavior is nonetheless
- * conventional and does not conflict with the expectations of ERC20
- * applications.
+ * We have followed general OpenZeppelin guidelines: functions revert instead
+ * of returning `false` on failure. This behavior is nonetheless conventional
+ * and does not conflict with the expectations of ERC20 applications.
  *
  * Additionally, an {Approval} event is emitted on calls to {transferFrom}.
  * This allows applications to reconstruct the allowance for all accounts just
@@ -35,11 +34,14 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     mapping(address => uint256) private _balances;
 
     mapping(address => mapping(address => uint256)) private _allowances;
+    //授权
 
     uint256 private _totalSupply;
+    //代币总量
 
-    string private _name;
-    string private _symbol;
+    string public _name;
+    //代币名称 简称
+    string public _symbol;
 
     /**
      * @dev Sets the values for {name} and {symbol}.
@@ -50,13 +52,10 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * All two of these values are immutable: they can only be set once during
      * construction.
      */
-	 
-    /*
-	constructor(string memory name_, string memory symbol_) {
-        _name = name_;
-        _symbol = symbol_;
-    }
-	*/
+    // constructor(string memory name_, string memory symbol_) {
+    //     _name = name_;
+    //     _symbol = symbol_;
+    // }
 
     /**
      * @dev Returns the name of the token.
@@ -76,7 +75,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
     /**
      * @dev Returns the number of decimals used to get its user representation.
      * For example, if `decimals` equals `2`, a balance of `505` tokens should
-     * be displayed to a user as `5.05` (`505 / 10 ** 2`).
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
      *
      * Tokens usually opt for a value of 18, imitating the relationship between
      * Ether and Wei. This is the value {ERC20} uses, unless this function is
@@ -87,7 +86,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * {IERC20-balanceOf} and {IERC20-transfer}.
      */
     function decimals() public view virtual override returns (uint8) {
-        return 18;
+        return 18;//这里是定义的精度，在发行的时候没有小数点这个概念，因此100...(18个)0000为1
     }
 
     /**
@@ -355,12 +354,14 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
         address to,
         uint256 amount
     ) internal virtual {}
-	
-	contract FECToken is ERC20{
+}
+
+contract FECToken is ERC20{
     
     constructor () public{
-        _name = "FECToken"
+        _name = "FECToken";
         _symbol = "FEC";
         _mint(msg.sender, 10000000000);
+        
     }
 }
